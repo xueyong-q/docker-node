620f2e98161e1b2f4d8abfa71b1b231476cced29
yong <freeyongx@163.com>
2021-03-15 01:06:03 +0800

    update

--- .devcontainer/ohmyzsh.sh
+++ .devcontainer/ohmyzsh.sh
@@ -3,7 +3,6 @@
 if [ ! -e ~/ohmyzsh.lock ]; then
     
     if [ -e ~/.oh-my-zsh ]; then rm -rf ~/.oh-my-zsh; fi
-    
     git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh && \
     cat ~/.oh-my-zsh/templates/zshrc.zsh-template >> ~/.zshrc && \
     echo "1" > ~/ohmyzsh.lock
